d0c44dc8fe14aa854fbc7f424df930b921f04766
JBushagour <joey.bushagour@gmail.com>
2019-07-30 12:04:44 -0400

    comments for socialdata

--- socialdata.py
+++ socialdata.py
@@ -1,22 +1,22 @@
 from UserProfile_model import UserProfile
 
 
-def save_profile(name, email, courses, school, groups):
-    p = get_user_profile(email)
-    if p:
+def save_profile(name, email, courses, school, groups): # Saves profile
+    p = get_user_profile(email) #gets the user profile
+    if p: #if it exists we update it
         p.name = name
         p.courses = courses
         p.school = school
-        if groups != "NOCHANGE":
+        if groups != "NOCHANGE": #don't change groups if it has "NOCHANGE"
             p.groups = groups
-    else:
-        if groups == "NOCHANGE":
+    else: #if it doens't we create it
+        if groups == "NOCHANGE": #if we create with a "mochange", then we must add an empty bracket
             groups = []
         p = UserProfile(email=email, name=name, courses=courses, school=school, groups=groups)
     p.put()
 
 
-def get_user_profile(email):
+def get_user_profile(email):  # gets user profile from email
     q = UserProfile.query(UserProfile.email == email)
     results = q.fetch(1)
     for profile in results:
@@ -24,7 +24,7 @@ def get_user_profile(email):
     return None
 
 
-def get_profile_by_name(name):
+def get_profile_by_name(name): # gets user profile from name
     q = UserProfile.query(UserProfile.name == name)
     results = q.fetch(1)
     for profile in results:
@@ -32,7 +32,7 @@ def get_profile_by_name(name):
     return None
 
 
-def get_profile_groups(email):
+def get_profile_groups(email):  # gets the groups in a profile
     q = UserProfile.query(UserProfile.email == email)
     results = q.fetch(1)
     for profile in results:
