457370af833842d5e3dca70ad5e950a1f717156a
JBushagour <joey.bushagour@gmail.com>
2019-07-30 11:52:12 -0400

    comments for this part of code

--- profile.py
+++ profile.py
@@ -3,29 +3,29 @@ import socialdata
 import helpers
 
 
-class ProfileEditHandler(webapp2.RequestHandler):
+class ProfileEditHandler(webapp2.RequestHandler): #Handles /profile-edit
     def get(self):
-        if not helpers.get_user_email():
+        if not helpers.get_user_email(): # if they don't have an email, we kick em
             self.redirect('/')
         else:
             values = helpers.get_template_parameters()
             profile = socialdata.get_user_profile(helpers.get_user_email())
-            if profile:
+            if profile:  # if they do have a profile, we populate it with default values
                 values['name'] = profile.name
                 values['school'] = profile.school
                 values['courses'] = profile.courses
-            helpers.render_template(self, 'profile-edit.html', values)
+            helpers.render_template(self, 'profile-edit.html', values)  # shows the profile-edit page
 
 
 class ProfileSaveHandler(webapp2.RequestHandler):
     def post(self):
         email = helpers.get_user_email()
-        if not email:
+        if not email:  # if they don't have an email, we kick em
             self.redirect('/')
         else:
-            error_text = ''
-            name = self.request.get("name")
-            school = self.request.get("school")
+            error_text = ''  # intialize an empty error text
+            name = self.request.get("name")  # gets values from form
+            school = self.request.get("school") 
             values = helpers.get_template_parameters()
             name.strip()
             name.replace(" ", "&")
@@ -42,7 +42,7 @@ class ProfileSaveHandler(webapp2.RequestHandler):
                 elif i == '.':
                     error_text += "Your name can't have ' . '\n"
             values['name'] = name
-            coursesList = []
+            coursesList = []  # code for showing repeated courses
             coursenum = 0
             course = self.request.get("classes0")
             while course:
@@ -51,24 +51,24 @@ class ProfileSaveHandler(webapp2.RequestHandler):
                 course = self.request.get("classes" + str(coursenum))
             values['courses'] = coursesList
             values['school'] = school
-            groups = "NOCHANGE"
+            groups = "NOCHANGE" #default group text, allows groups to not restart
             if error_text:
                 values['errormsg'] = error_text
             else:
-                socialdata.save_profile(name, email, coursesList, school, groups)
+                socialdata.save_profile(name, email, coursesList, school, groups)  # save profile if everything is fine
                 values['successmsg'] = "Everything worked out fine."
-            helpers.render_template(self, 'profile-edit.html', values)
+            helpers.render_template(self, 'profile-edit.html', values) #show page
 
 
-class ProfileViewHandler(webapp2.RequestHandler):
+class ProfileViewHandler(webapp2.RequestHandler):  #handles profile-view
     def get(self, profilename):
-        profile = socialdata.get_profile_by_name(profilename)
+        profile = socialdata.get_profile_by_name(profilename) #gets the profile
         values = helpers.get_template_parameters()
-        values['name'] = 'Unknown'
+        values['name'] = 'Unknown' #if the profile doens't exist we popoulate the spaces with defaults
         values['courses'] = ["courses does not exist"]
         values['school'] = "school does not exist"
-        if profile:
+        if profile:  # if we have a profile, populate the spaces with correct info
             values['name'] = profile.name
             values['courses'] = profile.courses
             values['school'] = profile.school
-        helpers.render_template(self, 'profile-view.html', values)
+        helpers.render_template(self, 'profile-view.html', values) #show page
