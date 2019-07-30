0ef11ef5b2aa85daae96fc4303b9ad4096d18345
JBushagour <joey.bushagour@gmail.com>
2019-07-30 12:02:13 -0400

    Comments on helpers

--- helpers.py
+++ helpers.py
@@ -3,12 +3,12 @@ from google.appengine.api import users
 from google.appengine.ext.webapp import template
 
 
-def render_template(handler, file_name, template_values):
+def render_template(handler, file_name, template_values):  # this is code written by a mythical man and we musn't touch it
     path = os.path.join(os.path.dirname(__file__), 'templates/' + file_name)
     handler.response.out.write(template.render(path, template_values))
 
 
-def get_user_email():
+def get_user_email():  # gets user's current email
     user = users.get_current_user()
     if user:
         return user.email()
@@ -16,7 +16,7 @@ def get_user_email():
         return None
 
 
-def get_template_parameters():
+def get_template_parameters():  # get's login or logout url
     values = {}
     if get_user_email():
         values['logout_url'] = users.create_logout_url('/')
