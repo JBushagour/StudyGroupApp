c57e3766ea598fa1a03fde9559fd88bc659f641e
JBushagour <joey.bushagour@gmail.com>
2019-07-30 10:25:42 -0400

    Comments added to group_data

--- group_data.py
+++ group_data.py
@@ -1,9 +1,9 @@
 from group_model import Group
 
 
-def save_group(name, description, course, member_limit, members, group_admin, school):
+def save_group(name, description, course, member_limit, members, group_admin, school): #this function saves the group
     p = get_group_by_name(name)
-    if p:
+    if p: #if it exists, we update the group
         p.name = name
         p.description = description
         p.course = course
@@ -11,7 +11,7 @@ def save_group(name, description, course, member_limit, members, group_admin, sc
         p.members = members
         p.group_admin = group_admin
         p.school = school
-    else:
+    else: #otherwise we create a new group 
         p = Group(
             name=name, description=description, course=course,
             member_limit=member_limit, members=members, group_admin=group_admin,
@@ -20,7 +20,7 @@ def save_group(name, description, course, member_limit, members, group_admin, sc
     p.put()
 
 
-def get_group_by_name(name):
+def get_group_by_name(name): # simple function to return group object given name
     q = Group.query(Group.name == name)
     results = q.fetch(1)
     for profile in results:
@@ -28,7 +28,7 @@ def get_group_by_name(name):
     return None
 
 
-def get_group_by_admin(email):
+def get_group_by_admin(email): # simple function to return group object given admin email
     q = Group.query(Group.group_admin == email)
     results = q.fetch(1)
     for profile in results:
