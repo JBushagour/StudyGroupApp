7c970bac04b885225bbb2ef441142b2a0f24f922
JBushagour <joey.bushagour@gmail.com>
2019-08-01 00:13:07 -0400

    unresolvable merge error

--- UserProfile_model.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import ndb
-
-
-class UserProfile(ndb.Model):
-    name = ndb.StringProperty(required=True)
-    email = ndb.StringProperty(required=True)
-    courses = ndb.StringProperty(repeated=True)
-    school = ndb.StringProperty(required=True)
